942babb2cbfdf4ac295aa1b3b494e8ed494161ca
PatrsiaRose <cseresznyevirag4@gmail.com>
2023-03-28 17:28:44 +0200

    gombok hozzáadása

--- script.js
+++ script.js
@@ -18,8 +18,14 @@ function toggleSwitchLetrehozasa() {
 
   text += `<div class="toggle-switch">`;
   text += `<input type="checkbox" id="toggle">`;
-  text += `<label for="toggle"></label>`;
+  text += `<label for="toggle">`
+  text += `<div class="buttons">`
+  text += `<button class="bal"></button>`
+  text += `<button class="jobb"></button>`
+  text += `</div>`;
+  text += `</label>`;
   text += `</div>`;
 
   return text;
 }
+ 
