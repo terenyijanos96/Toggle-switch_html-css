50b37bb71af699938f538a499f057fdff8f8a11e
Terényi János <terenyi.janos96@gmail.com>
2023-03-28 20:14:57 +0200

    Co-authored-by: PatrisaRose <PatrisaRose@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-window.addEventListener("load", main);
+window.addEventListener("load", init);
 
 let body;
 let szoveg;
+let tesztSzoveg;
 
 function init() {
   body = document.querySelector("body");
   szoveg = toggleSwitchLetrehozasa();
-}
-
-function main() {
-  init();
   body.innerHTML += szoveg;
+
+  tesztSzoveg = document.querySelector('body h1');
+  toggle = document.querySelector('input')
+  toggle.addEventListener("change", function (){
+    if (!toggle.checked) {
+      body.style.backgroundColor='white';
+      tesztSzoveg.style.color='black'
+      tesztSzoveg.innerText = "Reggel"
+    } else {
+      body.style.backgroundColor='black';
+      tesztSzoveg.style.color='white'
+      tesztSzoveg.innerText = "Este"
+    }
+  }) 
+
 }
 
 function toggleSwitchLetrehozasa() {
@@ -19,13 +31,13 @@ function toggleSwitchLetrehozasa() {
   text += `<div class="toggle-switch">`;
   text += `<input type="checkbox" id="toggle">`;
   text += `<label for="toggle">`
-  text += `<div class="buttons">`
-  text += `<button class="bal"></button>`
-  text += `<button class="jobb"></button>`
-  text += `</div>`;
+  text += `<div class="circle"></div>`
   text += `</label>`;
   text += `</div>`;
 
   return text;
 }
+
+
  
+
